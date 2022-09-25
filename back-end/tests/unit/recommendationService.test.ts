de9e1535b1c271d4769be72755ec21f4b2d3c6b3
Renan Araujo <renanaraujo852@hotmail.com>
2022-09-25 17:17:47 -0300

    chore: to do unit tests

---
+++ back-end/tests/unit/recommendationService.test.ts
@@ -0,0 +1,23 @@
+import { jest } from '@jest/globals';
+
+beforeEach(() => {
+    jest.resetAllMocks();
+    jest.clearAllMocks();
+});
+
+describe("Unit test for recommendation service", () =>{
+it.todo("Should create a recommendation")
+it.todo("Should not create a duplicated recommendation")
+it.todo("Should get recommendation by id")
+it.todo("Should get all recommendations")
+it.todo("Should get an amount recommendations ranked by votes")
+it.todo("Should add an upvote on the recommendation")
+it.todo("Should decrement an upvote on the recommendation")
+it.todo("Should get random recommendation")
+it.todo("Should remove recommendation if votes are less -5")
+it.todo("Should return gt if random params is less than 7")
+it.todo("Should return lte if random params is greater than 7")
+it.todo("Should return recomendation by score if recommendation length is greater than 0")
+it.todo("Should return all recomendation if recommendation length is 0")
+
+})
