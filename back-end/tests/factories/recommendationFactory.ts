4f3b71b37d884a9bc2498922e8335fddb654d67a
Renan Araujo <renanaraujo852@hotmail.com>
2022-09-26 00:06:51 -0300

    style: prettier format

--- back-end/tests/factories/recommendationFactory.ts
+++ back-end/tests/factories/recommendationFactory.ts
@@ -1,55 +1,55 @@
-import {faker} from "@faker-js/faker";
+import { faker } from "@faker-js/faker";
 
 function recommendationDataFactory() {
-    return {
-        "name": faker.lorem.lines(),
-        "youtubeLink": faker.internet.url()
-    }
+  return {
+    name: faker.lorem.lines(),
+    youtubeLink: faker.internet.url(),
+  };
 }
 function updatedRecommendationDataFactory() {
-    return {
-        "name": faker.lorem.lines(),
-        "youtubeLink": faker.internet.url(),
-        "score": faker.datatype.number({max: 1000})
-    }
+  return {
+    name: faker.lorem.lines(),
+    youtubeLink: faker.internet.url(),
+    score: faker.datatype.number({ max: 1000 }),
+  };
 }
 
 function hatedRecommendationDataFactory() {
-    return {
-        "name": faker.lorem.lines(),
-        "youtubeLink": faker.internet.url(),
-        "score": -1*(faker.datatype.number({min: 4}))
-    }
+  return {
+    name: faker.lorem.lines(),
+    youtubeLink: faker.internet.url(),
+    score: -1 * faker.datatype.number({ min: 4 }),
+  };
 }
 
 async function recommendationArrayFactory() {
-    const randNumber = faker.datatype.number({max: 20});
-    const recomendationArray = [];
-    for(let i = 0; i < randNumber; i++){
-        recomendationArray.push(recommendationDataFactory);
-    };
+  const randNumber = faker.datatype.number({ max: 20 });
+  const recomendationArray = [];
+  for (let i = 0; i < randNumber; i++) {
+    recomendationArray.push(recommendationDataFactory);
+  }
 
-    return recomendationArray;
+  return recomendationArray;
 }
 
 function numberFactory() {
-    return faker.datatype.number();
+  return faker.datatype.number();
 }
 
-function floatLessSevenFactory(){
-    return faker.datatype.float({max: 0.7})
+function floatLessSevenFactory() {
+  return faker.datatype.float({ max: 0.7 });
 }
 
-function scoreFilterFactory(){
-    return faker.helpers.arrayElement<"gt" | "lte">(["gt","lte"])
+function scoreFilterFactory() {
+  return faker.helpers.arrayElement<"gt" | "lte">(["gt", "lte"]);
 }
 
 export const recommendationFactories = {
-    recommendationDataFactory,
-    recommendationArrayFactory,
-    numberFactory,
-    updatedRecommendationDataFactory,
-    hatedRecommendationDataFactory,
-    floatLessSevenFactory,
-    scoreFilterFactory
-}
+  recommendationDataFactory,
+  recommendationArrayFactory,
+  numberFactory,
+  updatedRecommendationDataFactory,
+  hatedRecommendationDataFactory,
+  floatLessSevenFactory,
+  scoreFilterFactory,
+};
